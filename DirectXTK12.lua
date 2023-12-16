e035b66f62d73a084672442fbbb1cb51396bcdc2
ccameron2 <74011941+ccameron2@users.noreply.github.com>
2023-12-16 22:45:01 +0000

    Fixed premake file and removed XboxDDSLoader

--- DirectXTK12.lua
+++ DirectXTK12.lua
@@ -6,15 +6,20 @@ project "DirectXTK12"
     targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
+    pchheader "pch.h"
+    pchsource "Src/pch.cpp"
+
     includedirs
     {
-        
+        "inc/",
+        "src/",
+        "src/shaders/compiled"
     }
 
     files
     {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
+        "**.h",
+        "**.cpp"
     }
 
     filter "system:windows"
@@ -33,3 +38,8 @@ project "DirectXTK12"
         runtime "Release"
         optimize "On"
         symbols "Off"
+
+    removefiles("Src/pch.h")
+    removefiles("Src/pch.cpp")
+    removefiles("src/XboxDDSTextureLoader.cpp")
+    removefiles("Inc/XboxDDSTextureLoader.h")
